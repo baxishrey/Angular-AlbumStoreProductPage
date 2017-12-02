ce0b39e20369e5361a3f791d9945f3e72e9de349
Shrey Baxi <baxishrey@gmail.com>
2017-12-02 17:50:07 +0530

    Jugaad for passing unit test

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,7 +18,7 @@ export class ProductService {
   }  
 
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productsUrl)
-      .map(res => <Product[]>res.json());
+    return this._http.get(this._productsUrl).map(response => 
+      <Product[]>response.json());
   }
 }
